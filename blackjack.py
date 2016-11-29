38398bf628c6d41d54273d44b72cfdc9f3fa9206
Ayush Sagar <ayushsagar@gmail.com>
2016-11-29 17:22:25 -0500

    added speech reco code, commented everything else

--- blackjack.py
+++ blackjack.py
@@ -9,6 +9,92 @@ from naoqi import *
 import time
 
 
+class SpeechRecoModule(ALModule):
+    """ A module to use speech recognition """
+    def __init__(self, name):
+        ALModule.__init__(self, name)
+        try:
+            self._recognizedWord=[]
+            self._recognized_words_list=[]
+            self.asr = ALProxy("ALSpeechRecognition")
+            self._voc_dict=[]
+        except Exception as e:
+            self.asr = None
+        self.memory = ALProxy("ALMemory")
+        #print self.getName()
+
+    def onLoad(self):
+        from threading import Lock
+        self.bIsRunning = False
+        self.mutex = Lock()
+        self.hasPushed = False
+        self.hasSubscribed = False
+        self.BIND_PYTHON(self.getName(), "onWordRecognized")
+
+    def onUnload(self):
+        from threading import Lock
+        self.asr.pause(True)
+        self.mutex.acquire()
+        try:
+            if (self.bIsRunning):
+                if (self.hasSubscribed):
+                    self.memory.unsubscribeToEvent("WordRecognized", self.getName())
+                    #self.asr.pause(True)
+                if (self.hasPushed and self.asr):
+                    self.asr.popContexts()
+        except RuntimeError, e:
+            self.mutex.release()
+            raise e
+        self.bIsRunning = False;
+        self.mutex.release()
+
+    def onInput_onStart(self):
+        from threading import Lock
+        self.mutex.acquire()
+        if(self.bIsRunning):
+            self.mutex.release()
+            return
+        self.bIsRunning = True
+        try:
+            if self.asr:
+                self.asr.setVisualExpression(True)
+                self.asr.pushContexts()
+            self.hasPushed = True
+            if self.asr:
+                self.asr.setVocabulary( self._voc_dict, True )
+            self.memory.subscribeToEvent("WordRecognized", self.getName(), "onWordRecognized")
+            self.hasSubscribed = True
+        except RuntimeError, e:
+            self.mutex.release()
+            self.onUnload()
+            raise e
+        self.mutex.release()
+
+    def onWordRecognized(self, key, value, message):
+        #print 'word recognized'
+        #print key,'key'
+        #print value,'value'
+        #print message,'message'
+
+        if(len(value) > 1 and value[1] >= 0.45):
+            print 'recognized the word :', value[0]
+            self._recognizedWord=value
+            self._recognized_words_list.append(value[0])
+        else:
+            print 'speech was under minimum threshold'
+        self.memory.unsubscribeToEvent("WordRecognized", self.getName())
+        value[0] = ""
+    def getWord(self):
+        if self._recognizedWord == []:
+            return ""
+        return self._recognizedWord[0]
+    def getLastWord(self):
+        size=len(self._recognized_words_list)
+        return self._recognized_words_list[size-2]
+    def SetVocDict(self,voc):
+        self._voc_dict=voc
+        
+
 class Cards():
     cardValue = {
                 'spades2': 2,
@@ -167,13 +253,6 @@ class Cards():
         print "favorable cards:", favorableCardCount, " unseen cards:", unseenCardCount
         return favorableCardCount/float(unseenCardCount)
 
-ROBOT_IP="192.168.86.176"
-
-barcode=ALProxy("ALBarcodeReader", ROBOT_IP, 9559)
-tts = ALProxy("ALTextToSpeech", ROBOT_IP, 9559)
-barcode.subscribe("test_barcode")
-memory=ALProxy("ALMemory", ROBOT_IP, 9559)
-
 
 def listen(command):
     return None
@@ -215,28 +294,70 @@ def decision(cards):
 
     return outputStr        
 
-cardPrompts=["show me your first card", "show me your other card", "show me the dealer's card"]
+#ROBOT_IP="192.168.86.176"
+#ROBOT_IP="169.254.88.3"
+ROBOT_IP="192.168.0.101"
 
-while(1):
-    # Wait for "new game command"
-    listen("new game")
-    tts.say("okay...")
+barcode=ALProxy("ALBarcodeReader", ROBOT_IP, 9559)
+tts = ALProxy("ALTextToSpeech", ROBOT_IP, 9559)
+barcode.subscribe("test_barcode")
+memory=ALProxy("ALMemory", ROBOT_IP, 9559)
 
-    # instantite cards
-    cards = Cards()
-    time.sleep(1)
+broker = ALBroker("pythonBroker","0.0.0.0", 0, ROBOT_IP, 9559)
+pythonSpeechModule = SpeechRecoModule('pythonSpeechModule')
+pythonSpeechModule.onLoad()
+
+
+
+firstRun = True
+userDecision = None
+#while(1):
+#    listen("new game")
+
+# instantite cards
+cards = Cards()
+
+def recogSpeech(speechObj,tts,choices):
+    speechObj.SetVocDict(choices)
+    speechObj.onInput_onStart()
+    #pythonSpeechModule.sleep(10)
+    time.sleep(10)
+    wordList=speechObj.getWord().split("<...>")
+    print wordList
+    # if word recognized    
+    for word in wordList:
+        if word.strip() in choices:
+            return word.strip()
     
-    reco_card = seeCard(cards, "show me your first card")
-    if reco_card in Cards.cardValue:
-        tts.say("I see, a " + cardDesc(reco_card) + ";")
+    # if word not recognized, but new game    
+    for word in wordList:
+        if word.strip() == "new game":
+            return -1
 
-    reco_card = seeCard(cards, "show me your second card")
-    if reco_card in Cards.cardValue:
-        tts.say("I see, a " + cardDesc(reco_card) + ";")
+    # else try again
+    tts.say(".")
+    recogSpeech(speechObj, tts, choices)
 
-    reco_card = seeCard(cards, "show me the dealer's card",isDealerCard=True)
-    if reco_card in Cards.cardValue:
-        tts.say("I see, a " + cardDesc(reco_card) + ";")
+#while(firstRun == True or (userDecision=='stay' and cards.total()<21)):
+# 
+#    firstRun = False   
+#    tts.say("okay...")
+#
+#    time.sleep(1)
+#    
+#    reco_card = seeCard(cards, "show me your first card")
+#    if reco_card in Cards.cardValue:
+#        tts.say("I see, a " + cardDesc(reco_card) + ";")
+#
+#    reco_card = seeCard(cards, "show me your second card")
+#    if reco_card in Cards.cardValue:
+#        tts.say("I see, a " + cardDesc(reco_card) + ";")
+#
+#    reco_card = seeCard(cards, "show me the dealer's card",isDealerCard=True)
+#    if reco_card in Cards.cardValue:
+#        tts.say("I see, a " + cardDesc(reco_card) + ";")
+#
+#    tts.say(decision(cards))
+#    break
 
-    tts.say(decision(cards))
-    break
+pythonSpeechModule.onUnload()
